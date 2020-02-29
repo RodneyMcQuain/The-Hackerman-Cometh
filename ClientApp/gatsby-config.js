b8ce329a10a6acd2707e2c9d6fb060c97a7d8a03
Rodney McQuain <rodneymcquain95@gmail.com>
2020-02-28 21:42:46 -0500

    feat: add emotion (CSS in JS)

--- ClientApp/gatsby-config.js
+++ ClientApp/gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  siteMetadata: {
-    title: 'ASP.NET Core Gatsby',
-  },
-  plugins: ['gatsby-plugin-react-helmet']
+    siteMetadata: {
+        title: `ASP.NET Core Gatsby`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-emotion`,
+    ]
 }
