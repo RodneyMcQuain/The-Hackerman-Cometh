22a6a95bebe7dd071a6d2e456441d63294ec509b
Zachary Thomas <thomzt01@pfw.edu>
2020-03-03 13:01:57 -0500

    wip: Connects to the DB and communicates with it.

---
+++ Model/SeedData.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcUsers.Model
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcUsersContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcUsersContext>>()))
+            {
+                // Look for any users.
+                if (context.Users.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
