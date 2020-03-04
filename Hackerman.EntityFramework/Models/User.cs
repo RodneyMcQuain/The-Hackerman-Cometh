d8370bc35cabe483195ff6c6687cad9847c8f411
Zachary Thomas <thomzt01@pfw.edu>
2020-03-04 12:55:01 -0500

    feat: add database connection and User model

--- Hackerman.EntityFramework/Models/User.cs
+++ Hackerman.EntityFramework/Models/User.cs
@@ -4,9 +4,9 @@ namespace Hackerman.EntityFramework.Models
 {
     public class User
     { 
+        public long ID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public string HashPassword { get; set; }
     }
 }
