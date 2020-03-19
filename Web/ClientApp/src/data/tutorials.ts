a6e9a4b000a3e71537c27942c0c38103d1a0fc44
Zachary Thomas <thomzt01@pfw.edu>
2020-03-19 12:30:16 -0400

    fix: quality of life changes

--- Web/ClientApp/src/data/tutorials.ts
+++ Web/ClientApp/src/data/tutorials.ts
@@ -1,8 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var antd_1 = require("antd");
-var Tutorial = [
+﻿export const tutorials = [
     {
         name: "Server-Side Includes Injection",
         description: "A server side injection is a very simple hacking method, the user inputs simple Linux commands into any given text input and as a result the command is executed. Commonly used for gaining access to local file systems.",
@@ -17,13 +13,6 @@ var Tutorial = [
         name: "Review SSI and SQL",
         description: "This tutoial is a review of the prior learned skills with less guidance.",
         link: "review0-Tutorial",
+
     }
-];
-var Tutorials = function () { return (React.createElement(antd_1.Layout, null,
-    React.createElement(antd_1.Row, { gutter: 16, justify: "center" }, Tutorial.map(function (Tutorial) { return (React.createElement(antd_1.Col, { span: 8, flex: "auto" },
-        React.createElement(antd_1.Card, { title: Tutorial.name, bordered: false },
-            React.createElement("span", null, Tutorial.description),
-            React.createElement("br", null),
-            React.createElement(antd_1.Button, { type: "primary", href: Tutorial.link }, "Start")))); })))); };
-exports.default = Tutorials;
-//# sourceMappingURL=tutorials.js.map
+]
--- Web/ClientApp/src/pages/tutorials.tsx
+++ Web/ClientApp/src/pages/tutorials.tsx
@@ -1,7 +1,9 @@
 ﻿import * as React from 'react'
+import { Link } from 'gatsby'
 import { Card, Col, Row, Button } from 'antd'
 import Layout from '../components/Layout/layout'
 import Banner from '../components/Index/Banner'
+import { tutorials } from '../data/tutorials'
 
 interface Tutorial {
     name: string,
@@ -9,35 +11,19 @@ interface Tutorial {
     link: string
 }
 
-const Tutorial = [
-    {
-        name: "Server-Side Includes Injection",
-        description: "A server side injection is a very simple hacking method, the user inputs simple Linux commands into any given text input and as a result the command is executed. Commonly used for gaining access to local file systems.",
-        link: "ssi-injection",
-    },
-    {
-        name:"Structured Query Language Injection",
-        description:"An SQL (Structured Query Language) Injection is a simple cyber attack, the hacker inputs a query that will return true and in return gain access to valuable information.",
-        link:"sql-injection",
-    },
-    {
-    name:"Review SSI and SQL",
-    description: "This tutoial is a review of the prior learned skills with less guidance.",
-    link: "review0-Tutorial",
-
-    }
-]
 
 const Tutorials = () => (
     <Layout>
         <Row gutter={16} justify="center">
-            {Tutorial.map(Tutorial => (
+            {tutorials.map(tutorial => (
                 <Col span={8} flex="auto">
-                    <Card title={Tutorial.name} bordered={false}>
-                        <span>{Tutorial.description}</span>
-                        <br></br>
-                        <Button type="primary" href={Tutorial.link}>
+                    <Card title={tutorial.name} bordered={false}>
+                        <span>{tutorial.description}</span>
+                        <br />
+                        <Button type="primary">
+                            <Link to={tutorial.link} >
                                 Start
+                            </Link>
                         </Button>
                        
                     </Card>
