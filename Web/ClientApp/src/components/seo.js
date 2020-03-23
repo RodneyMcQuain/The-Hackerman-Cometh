fa75c7106f306067cdb9d0dfd5953e808b3a20a4
Zachary Thomas <thomzt01@pfw.edu>
2020-03-23 11:10:22 -0400

    wip: increase our SEO and add robots.txt

---
+++ Web/ClientApp/src/components/seo.js
@@ -0,0 +1,93 @@
+﻿import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+function SEO({ description, lang, meta, title, image }) {
+    const { site } = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                        description
+                        author
+                        image
+                        siteUrl
+                    }
+                }
+            }
+        `
+    );
+
+    const metaDescription = description || site.siteMetadata.description;
+    const metaImage = image || site.siteMetadata.image;
+    const imageUrl = `${site.siteMetadata.siteUrl}/${metaImage}`;
+
+    return (
+        <Helmet
+            htmlAttributes={{
+                lang,
+            }}
+            title={title}
+            titleTemplate={`%s | ${site.siteMetadata.title}`}
+            meta={[
+                {
+                    name: `description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:title`,
+                    content: `${title} | ${site.siteMetadata.title}`,
+                },
+                {
+                    property: `og:description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:image`,
+                    content: imageUrl,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:card`,
+                    content: `summary`,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: site.siteMetadata.author,
+                },
+                {
+                    name: `twitter:title`,
+                    content: title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: metaDescription,
+                },
+                {
+                    name: `twitter:image`,
+                    content: imageUrl,
+                },
+            ].concat(meta)}
+        />
+    );
+}
+
+SEO.defaultProps = {
+    lang: `en`,
+    meta: [],
+    description: ``,
+}
+
+SEO.propTypes = {
+    description: PropTypes.string,
+    lang: PropTypes.string,
+    meta: PropTypes.arrayOf(PropTypes.object),
+    title: PropTypes.string.isRequired,
+}
+
+export default SEO;
